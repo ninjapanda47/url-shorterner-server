ac9e250a6cf95b77afbd64eb5af98f4efa47e850
Sandi <sandi.c.gau@gmail.com>
2019-06-22 17:57:37 -0500

    push to heroku

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json());
 require("./models/UrlShorten");
 require("./routes/urlshorten")(app);
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`Server started on port`, PORT);
 });
